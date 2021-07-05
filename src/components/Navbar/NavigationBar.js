ea3c36692b0d12ac2c60bf06f72bd9eae8dad829
carl-bernhardhallberg <cbha@kth.se>
2021-07-05 13:53:42 +0200

    - Dropdown implemented - Linking to Git, Discord etc.

--- src/components/Navbar/NavigationBar.js
+++ src/components/Navbar/NavigationBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { items } from "./Items";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import style from './NavigationBar.css';
 import {Link} from 'react-scroll';
 import { useEffect } from 'react';
@@ -14,17 +14,19 @@ class NavigationBar extends React.Component{
                 <div className="Menu">
                 </div>
                 <ul className="nav-items">
-
-                    {items.map((item, index) => {
-                        return(
-                            <li key={index}>
-                                <Link className={item.name} to={item.url} smooth={true} duration={1000}>
-                                    {item.title}
-                                </Link>
-                            </li>
-                        )
-
-                    })}
+                    <Link className="nav-links" to="tokenomics" smooth={true} duration={1000}>Tokenomics</Link>
+                    <Link className="nav-links" to="about" smooth={true} duration={1000}>About</Link>
+                    <Link className="nav-links" to="team" smooth={true} duration={1000}>Team</Link>
+                    <Link className="nav-links" to="roadmap" smooth={true} duration={1000}>Road Map</Link>
+                    <NavDropdown className="nav-links" title="Docs" id="collapsible-nav-dropdown" style={{textDecoration: 'none'}}>
+                        <NavDropdown.Item className="nav-links" href="https://docs.google.com/document/d/1m179syu3jzJltHuWAE_LU6h-bzHWharmgzZzcvOTKd0/edit" >Whitepaper</NavDropdown.Item>
+                        <NavDropdown.Divider></NavDropdown.Divider>
+                        <NavDropdown.Item className="nav-links" href="https://github.com/Maraboue/DynamicNetworkToken">Github</NavDropdown.Item>
+                        <NavDropdown.Divider></NavDropdown.Divider>
+                        <NavDropdown.Item className="nav-links" href="https://discord.gg/5pYJkvs4">Discord</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item className="nav-links" href="#action/3.4">Donate to dag</NavDropdown.Item>
+                    </NavDropdown>
                 </ul>
             </nav>
         );
